a512d8589daabd62144c8f8aa0cd46ffd5c23ca1
axnovot <axnovothny@gmail.com>
2024-07-06 18:42:02 -0400

    create tctcpclient.h header file and add header to tctcpclient.cpp. Test tcp_client in TWSclient.cpp

--- tclogger/TWSclient.cpp
+++ tclogger/TWSclient.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "tclogger.h"
-
+#include "tctcpclient.h"
 
 int main(){
     ELOG << "Hello World" << endtl;
@@ -9,7 +9,12 @@ int main(){
 
     ELOG << "Have a nice day" << endtl;
 
-    cout << "Message Using Cout" << endtl; 
+    cout << "Message Using Cout" << endtl;
+
+    int result = tcp_client();
+    if(result != 0){
+        cerr << "TCP client encountered an error :( " << endl;
+    }
 }
 
 
---
+++ tclogger/tctcpclient.h
@@ -0,0 +1,6 @@
+#ifndef TCP_CLIENT_H
+#define TCP_CLIENT_H
+
+int tcp_client();
+
+#endif
